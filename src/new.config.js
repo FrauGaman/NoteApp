b0c23c608c4d1751e04f02cd74207ccad02ad32c
FrauGaman <Gamanm@yandex.ru>
2019-06-05 13:40:31 +0700

    Delete new.config.js

--- src/new.config.js
+++
@@ -1,9 +0,0 @@
-//сюда будем экспортировать опции, которые хотип применить
-module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-		? ''
-		: '/',
-	assetsDir: '',
-	productionSourceMap: false
-
-}
